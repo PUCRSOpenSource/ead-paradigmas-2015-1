8edc9b791fcbf3586f62a05ed739f6e1d2c82b23
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2015-06-21 18:12:44 -0300

    Starts t7

---
+++ t7/t7.pl
@@ -0,0 +1,13 @@
+%prolog
+:-
+	nl,
+	write('+------------------------------------------+'),
+	nl,
+	write('| Paradigmas das Linguagens de Programação |'),
+	nl,
+	write('|           Semestre: 2015/1               |'),
+	nl,
+	write('|     Aluno: Diego Pinto da Jornada        |'),
+	nl,
+	write('+------------------------------------------+').
+	
